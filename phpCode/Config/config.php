275f789a1e378d6ec392bd005d7cf514a344a951
DebanhiRosiles <DebanhiRosiles@gmail.com>
2020-07-13 21:33:59 -0500

    Eliminar dup

--- phpCode/Config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-      define('HOST','localhost');
-      define ('USER_NAME','root');
-      define('PASSWORD','');
-      define('BD', 'prueba');
-?>
--- phpCode/eliminaP.php
+++
@@ -1,9 +0,0 @@
-<?php 
-!isset($_POST)? die('Acceso denegado'):'';
-    require 'conexion.class.php';
-    $conexion = new Conexion();
-$clve_Productos=$_POST['id'];
-
-$sql="DELETE FROM `productos` WHERE `clve_Productos`=$clve_Productos";
-echo $resultado= mysqli_query($conexion,$sql);
-?>
